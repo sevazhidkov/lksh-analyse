7298ba5395ea870eac3894c0d09d75ff31cb1e20
Seva Zhidkov <zhidkovseva@gmail.com>
2015-12-22 11:35:05 +0400

    Improve parsing script

--- parse_poldnev.py
+++ parse_poldnev.py
@@ -15,6 +15,7 @@ import requests
 import lxml.html
 
 POLDNEV_BASE_URL = 'http://poldnev.ru/lksh/id{}'
+LAST_STUDENT_ID = 3024
 STUDENT_IMAGE_REGEX = re.compile(r'href=\\"https?://img-fotki.yandex.ru/get/.*_XXL')
 OCP_API_KEY = os.environ['OCP_API_KEY']
 EMOTION_API_URL = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
@@ -22,12 +23,14 @@ EMOTION_API_URL = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
 
 def detect_emotions(photo_url):
     # Emotion API limits
-    time.sleep(2)
+    time.sleep(2.5)
     response = requests.post(EMOTION_API_URL, headers={
         'Content-Type': 'application/json',
         'Ocp-Apim-Subscription-Key': OCP_API_KEY
     }, data=json.dumps({'url': photo_url}))
     faces = json.loads(response.text)
+    if not faces:
+        return None
     # If there are few faces, sort it by square of face rectangle
     faces.sort(
         key=lambda x: x['faceRectangle']['width'] * x['faceRectangle']['height'],
@@ -36,16 +39,17 @@ def detect_emotions(photo_url):
     return list(faces[0]['scores'].values())
 
 print('Starting analysing data from poldnev.ru')
-visits_file = open('visits.csv', 'w')
+visits_file = open('visits.csv', 'a')
 visits_writer = csv.writer(visits_file)
 # First student id
 student_id = 1
-while True:
+while student_id <= LAST_STUDENT_ID:
     student_page = requests.get(POLDNEV_BASE_URL.format(student_id)).text
     student_html = lxml.html.document_fromstring(student_page)
     student_name = str(student_html.find_class('header-center').pop().text_content())
     if 'Ошибка' in student_name:
-        break
+        student_id += 1
+        continue
     student_photos = []
     for photo in re.findall(STUDENT_IMAGE_REGEX, student_page):
         # Delete a beginning ('href="') of the string
@@ -58,10 +62,21 @@ while True:
         visit_date = row[1].text_content()
         visit_position = row[2].text_content()
         # If there is no photo for this visit - continue to next student visit
-        if 'class' not in row.attrib:
+        if 'class' not in row.attrib and not len(student_photos) == 1:
             continue
+        try:
+            emotions = detect_emotions(student_photos[photo_num])
+        except IndexError:
+            break
+        if not emotions:
+            photo_num += 1
+            continue
+        visits_writer.writerow(
+            [student_id, student_name, visit_date,
+             visit_position] + emotions
+        )
         print(student_id, student_name, visit_date,
-              visit_position, detect_emotions(student_photos[photo_num]))
+              visit_position, emotions, sep='|')
         photo_num += 1
     student_id += 1
 
